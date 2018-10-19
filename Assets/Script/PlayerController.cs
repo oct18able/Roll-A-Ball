8f7678fb5c6c0700a0ec9132d0223dcc17feba23
oct18able <43513589+oct18able@users.noreply.github.com>
2018-10-19 15:19:36 +0900

    test

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float speed = 10;
+
+    private void FixedUpdate()
+    {
+        //入力をx,zに代入
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //同一のGameObjectが持つRigidBodyコンポーネントの取得
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        //rigidbodyのxとy行くに力を加える
+        rigidbody.AddForce(x*speed, 0, z*speed);
+
+    }
+
+}
